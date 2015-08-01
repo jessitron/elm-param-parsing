6e5a1f43952dd2578b270e13d02f24e6fc813d49
Jessica Kerr <jessitron@outpace.com>
2015-08-01 17:53:50 +0000

    there is a bug

--- UrlParams.elm
+++ UrlParams.elm
@@ -35,5 +35,20 @@ parseParams : String -> Model
 parseParams stringWithAmpersands =
   let
     eachParam = (String.split "&" stringWithAmpersands)
+    eachPair  = List.map (splitAtFirst '=') eachParam
   in
-    Error ("Got: " ++ (String.join ", " eachParam))
+    Error ("Got: " ++ (String.join ", " (List.map toString eachPair)))
+
+
+splitAtFirst : Char -> String -> (String, String)
+splitAtFirst c s =
+  case (firstOccurrence c s) of
+    Nothing -> (s, "")
+    Just i  -> ((String.left i s), (String.dropLeft (i + 1) s))
+
+
+firstOccurrence : Char -> String -> Maybe Int 
+firstOccurrence c s = 
+  case (String.indexes (toString c) s) of
+    []        -> Nothing
+    head :: _ -> Just head
