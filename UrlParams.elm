9f55444c3034809d54ca6852bfcae58e34324f2f
Jessica Kerr <jessitron@outpace.com>
2015-08-01 15:33:15 +0000

    Define a model and a view

--- UrlParams.elm
+++ UrlParams.elm
@@ -4,7 +4,22 @@ import Html exposing (Html)
 import Dict exposing (Dict)
 import String
 
+--- MODEL
+type Model = 
+    Error String
+  | UrlParams (Dict String String)
+
+model = Error windowLocationSearch
+
+--- VIEW 
+view : Model -> Html
+view model = 
+  case model of
+    Error msg -> Html.h2 [] [Html.text msg]
+    UrlParams _ -> Html.text "Not implemented"
+
+--- wiring
 main : Html
-main = Html.text windowLocationSearch
+main = view model
 
 port windowLocationSearch : String
